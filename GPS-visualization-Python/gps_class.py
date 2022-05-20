319c3d11ec468f38c8c0f8da1810f57202ff8079
FelipMarti <fmarti@swin.edu.au>
2022-05-20 17:04:12 +1000

    Adding library to plot GPS routes

---
+++ GPS-visualization-Python/gps_class.py
@@ -0,0 +1,108 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from PIL import Image, ImageDraw
+
+
+class GPSVis(object):
+    """
+        Class for GPS data visualization using pre-downloaded OSM map in image format.
+    """
+    def __init__(self, title, map_path, points):
+        """
+        :param data_path: Path to file containing GPS records.
+        :param map_path: Path to pre-downloaded OSM map in image format.
+        :param points: Upper-left, and lower-right GPS points of the map (lat1, lon1, lat2, lon2).
+        """
+        self.data_path = '' 
+        self.title = title
+        self.points = points
+        self.map_path = map_path
+        self.legend = []
+
+        self.result_image = Image.open(self.map_path, 'r')
+        self.x_ticks = []
+        self.y_ticks = []
+
+    def plot_map(self, output='save', save_as='resultMap.png'):
+        """
+        Method for plotting the map. You can choose to save it in file or to plot it.
+        :param output: Type 'plot' to show the map or 'save' to save it.
+        :param save_as: Name and type of the resulting image.
+        :return:
+        """
+        #self.get_ticks()
+        fig, axis1 = plt.subplots(figsize=(10, 10))
+        axis1.imshow(self.result_image)
+        axis1.set_xlabel('Longitude')
+        axis1.set_ylabel('Latitude')
+        axis1.set_xticklabels(self.x_ticks)
+        axis1.set_yticklabels(self.y_ticks)
+        axis1.grid()
+        axis1.legend(handles=self.legend, loc='center right')
+        title = self.title
+        axis1.title.set_text(title)
+        if output == 'save':
+            plt.savefig(save_as , format='eps', dpi=800)
+        else:
+            plt.show()
+
+    def create_image(self, data_path, color, width=2):
+        """
+        Create the image that contains the original map and the GPS records.
+        :param color: Color of the GPS records.
+        :param width: Width of the drawn GPS records.
+        :return:
+        """
+        data = pd.read_csv(data_path, names=['TIMESTAMP', 'DOW', 'YEAR', 'MONTH', 'DAY', 'H', 'M', 'S', 'TZ', 'LATITUDE', 'LONGITUDE', 'SPEED', 'TRUCK'], sep=',')
+
+        #self.result_image = Image.open(self.map_path, 'r')
+        img_points = []
+        legend = data['TRUCK'][0]
+        gps_data = tuple(zip(data['LATITUDE'].values, data['LONGITUDE'].values))
+        for d in gps_data:
+            # Filtering out invalid GPS data indicated as > 90 and > 180
+            if  (d[0] <= 90) and (d[1] <= 180) :
+                x1, y1 = self.scale_to_img(d, (self.result_image.size[0], self.result_image.size[1]))
+                img_points.append((x1, y1))
+
+        patch = mpatches.Patch(color=color, label=legend)
+        self.legend.append(patch)
+        draw = ImageDraw.Draw(self.result_image)
+        draw.line(img_points, fill=color, width=width)
+
+    def scale_to_img(self, lat_lon, h_w):
+        """
+        Conversion from latitude and longitude to the image pixels.
+        It is used for drawing the GPS records on the map image.
+        :param lat_lon: GPS record to draw (lat1, lon1).
+        :param h_w: Size of the map image (w, h).
+        :return: Tuple containing x and y coordinates to draw on map image.
+        """
+        # https://gamedev.stackexchange.com/questions/33441/how-to-convert-a-number-from-one-min-max-set-to-another-min-max-set/33445
+        old = (self.points[2], self.points[0])
+        new = (0, h_w[1])
+        y = ((lat_lon[0] - old[0]) * (new[1] - new[0]) / (old[1] - old[0])) + new[0]
+        old = (self.points[1], self.points[3])
+        new = (0, h_w[0])
+        x = ((lat_lon[1] - old[0]) * (new[1] - new[0]) / (old[1] - old[0])) + new[0]
+        # y must be reversed because the orientation of the image in the matplotlib.
+        # image - (0, 0) in upper left corner; coordinate system - (0, 0) in lower left corner
+        return int(x), h_w[1] - int(y)
+
+    def get_ticks(self):
+        """
+        Generates custom ticks based on the GPS coordinates of the map for the matplotlib output.
+        :return:
+        """
+        self.x_ticks = map(
+            lambda x: round(x, 4),
+            np.linspace(self.points[1], self.points[3], num=7))
+        y_ticks = map(
+            lambda x: round(x, 4),
+            np.linspace(self.points[2], self.points[0], num=8))
+        # Ticks must be reversed because the orientation of the image in the matplotlib.
+        # image - (0, 0) in upper left corner; coordinate system - (0, 0) in lower left corner
+        self.y_ticks = sorted(y_ticks, reverse=True)
---
+++ GPS-visualization-Python/main.py
@@ -0,0 +1,77 @@
+from gps_class import GPSVis
+
+import sys, getopt, os
+
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    inputfolder = ''
+    graph_title ='' 
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:f:",["ifile=","ofile=","ifolder="])
+    except getopt.GetoptError:
+        print ('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-f", "--ifolder"):
+            inputfolder = arg
+    if not inputfile and not inputfolder:
+        print ('test.py -i <inputfile> -o <outputfile>')
+        print ('or')
+        print ('test.py -f <inputfolder> -o <outputfile>')
+        sys.exit(2)
+
+    if inputfile:
+        print('Input file is ' + inputfile)
+        graph_title = os.path.splitext(inputfile)[0]
+    elif inputfolder:
+        graph_title = inputfolder
+        print('Input folder is ' + inputfolder)
+
+    if not outputfile and inputfile:
+        outputfile=os.path.splitext(inputfile)[0]
+        outputfile=outputfile + '.eps'
+    elif not outputfile and inputfolder:
+        outputfile=os.path.abspath(inputfolder) + '.eps'
+    print('Output file is ' + outputfile)
+
+    vis = GPSVis(title=graph_title,
+            map_path='map.png',  # Path to map downloaded from the OSM.
+            points=(-37.68, 144.75, -37.825, 144.89)) # Two coordinates of the map (upper left, lower right)
+
+    if inputfile:
+        vis.create_image(data_path=inputfile, color=(0, 0, 255), width=3)  # Set the color and the width of the GNSS tracks.
+
+    elif inputfolder:
+        colours = ['blue', 'red', 'orange', 'darkgreen' ,'magenta', 'purple', 'coral', 'cornflowerblue', 'limegreen', 'darkorchid', 'gold', 'steelblue', 'yellowgreen', 'chocolate', 'teal']
+        rotate_colour = 0
+
+        file_list = os.listdir(inputfolder)
+        for filename in sorted(file_list):
+            inputfile = inputfolder + '/' + filename
+
+            if os.path.splitext(inputfile)[1] == '.log':
+                print (inputfile)
+                vis.create_image(data_path=inputfile, color=(colours[rotate_colour]), width=3)  # Set the color and the width of the GNSS tracks.
+                rotate_colour += 1
+            else:
+                print ('Ignoring file with wrong extension:' + inputfile)
+            
+
+    vis.plot_map(output='save', save_as=outputfile)
+
+    print()
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
